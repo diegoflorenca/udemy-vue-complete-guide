d000b3422cbd8a759ed57869b847a233b52e5298
Diego Felipe Florença <diegoflorenca@gmail.com>
2022-05-09 11:29:04 -0700

    7. Re-building the App with Vue

--- 01-starting-project/app.js
+++ 01-starting-project/app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+	data() {
+		return {
+			goals: [],
+			enteredValue: '',
+		};
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredValue);
+			this.enteredValue = '';
+		},
+	},
+}).mount('#app');
 
-function addGoal() {
-	const enteredValue = inputEl.value;
-	const listItemEl = document.createElement('li');
-	listItemEl.textContent = enteredValue;
-	listEl.append(listItemEl);
-	inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal);
+// function addGoal() {
+// 	const enteredValue = inputEl.value;
+// 	const listItemEl = document.createElement('li');
+// 	listItemEl.textContent = enteredValue;
+// 	listEl.append(listItemEl);
+// 	inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
